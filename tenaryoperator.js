7e6b39b3a66e78c98460cea20f40952b1675cd34
idehenpj <idehenp@gmail.com>
2025-03-03 12:55:17 +0000

    new file was created

--- tenaryoperator.js
+++ tenaryoperator.js
@@ -1,4 +1,4 @@
-/*let grade = 70
+let grade = 70
 let result;
 
 if (grade>= 70 && grade <=100){
@@ -7,8 +7,10 @@ if (grade>= 70 && grade <=100){
     result= "Do better next time";
 }
 result = (grade >= 70 && grade <= 100)? "A": "Do better next time";
-console.log(result);*/
+console.log(result);
 
 for(let i = 2; i <=20; i+=2) {
     console.log(i);
 }
+
+
